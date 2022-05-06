9aae2277fa5b32956b8943e34c009dea79c799a5
findanna <anna.mantsoki@finddx.org>
2022-05-06 09:34:51 +0000

    Commit inflated package for function-a

---
+++ R/data_weight.R
@@ -0,0 +1,13 @@
+#' Data for height and mass of Starwars heroes
+#'
+#' A dataset containing information for height and mass of Starwars heroes
+#'
+#' @format A data frame with 87 rows and 3 variables:
+#' \describe{
+#' \item{name}{name of the hero}
+#' \item{height}{height of the hero, in m}
+#' \item{mass}{mass of the hero, in Kg}
+#' ...
+#' }
+#' @source \url{https://dplyr.tidyverse.org/reference/starwars.html}
+"data_weight"
